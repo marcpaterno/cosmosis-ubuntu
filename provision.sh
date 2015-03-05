9384fe85d60b4797b4adac2d6d3713591ece3fa4
Marc Paterno <paterno@fnal.gov>
2015-03-05 11:19:42 -0600

    Use system git

--- provision.sh
+++ provision.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
+function install_pkg()
+{
+    (( $# > 1 )) || return 1 # Need args
+    echo "Installing apt package $1"
+    apt-get install -y $1 >/dev/null 2>&1
+}
+
+echo "Updating apt package list"
 apt-get update >/dev/null 2>&1
-apt-get install -y curl  # needed for downloading UPS tarballs
-apt-get install -y g++   # needed to build cmake
-apt-get install -y libc6-dev-i386 # needed to build gcc
 
-mkdir -p /products
+install_pkg curl
+install_pkg g++             # for gcc
+install_pkg libc6-dev-i386  # for gcc
+install_pkg zlib1g-dev      # for gcc
+
+install_pkg texinfo         # for gdb
+install_pkg libncurses5-dev # for gdb
+
+install_pkg git
+
+echo "Creating /products directory"
+mkdir -p /products >/dev/null 2>&1
 pushd /products
-curl --fail -O \
-     http://scisoft.fnal.gov/scisoft/packages/ups/v5_1_3/ups-5.1.3-Linux64bit%2B3.13-2.19.tar.bz2
 
+echo "Installing curl product"
+curl --fail -O http://scisoft.fnal.gov/scisoft/packages/ups/v5_1_3/ups-5.1.3-Linux64bit+3.13-2.19.tar.bz2  >/dev/null 2>&1
 tar xf ups-5.1.3-Linux64bit+3.13-2.19.tar.bz2
 
-chown -R vagrant:vagrant /products
+echo "Setting ownership of products"
+chown -R vagrant:vagrant /products >/dev/null 2>&1
+
